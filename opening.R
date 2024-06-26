f5fe14b12da2b122bb197c8c3a85077f81506b0b
satyavoludinesh <dineshsatyavolu2007@gmail.com>
2024-06-26 19:32:51 -0400

    patching up project

---
+++ opening.R
@@ -0,0 +1,4 @@
+# Chess computers across decades have reached unprecedented strengths, starting with the success of IBM computer Deep Blue. 
+# A data set was sourced from CCRL (Computer Chess Rating Lists) to measure these developments. 
+# The data was then used to graph out the changes in rating of top chess computers in an analytical format. 
+# With rigorous analysis, it is noted that chess computers are several hundred rating points higher than humans, and now all compete among themselves with unorthodox styles.
